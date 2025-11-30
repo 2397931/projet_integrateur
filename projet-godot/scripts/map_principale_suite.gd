7f8c345e77f0e5d551ce574d9b8155507d3cea03
2397931 <143234904+2397931@users.noreply.github.com>
2025-11-30 18:45:07 -0500

    avancer reacteur

--- projet-godot/scripts/map_principale_suite.gd
+++ projet-godot/scripts/map_principale_suite.gd
@@ -3,11 +3,11 @@ extends Node2D
 func _ready() -> void:
 	global.current_scene = "map_principale_suite"
 	if global.game_first_loadin == true:
-		$joueur.position.x = global.joueur_start2_posx
-		$joueur.position.y = global.joueur_start2_posy
-	else:
 		$joueur.position.x = global.joueur_exit_infirmerie_posx
 		$joueur.position.y = global.joueur_exit_infirmerie_posy
+	else:
+		$joueur.position.x = global.joueur_start2_posx
+		$joueur.position.y = global.joueur_start2_posy
 
 func _process(delta: float) -> void:
 	change_scene()
